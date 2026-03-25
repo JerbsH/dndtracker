499de6fc2ed29763132bec16013594d3b8412a6b
JerbsH <hippelainen.jere@gmail.com>
2026-03-25 14:35:51 +0200

    Add landing page and placeholder cards

--- app/page.tsx
+++ app/page.tsx
@@ -8,31 +8,37 @@ import {
 
 export default function Home() {
   return (
-    <div className="flex flex-row flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Card className="w-1/2 max-w-sm mx-2 p-2">
+    <div className="flex flex-row flex-1 items-center justify-center font-sans gap-4 bg-[url(https://cdn.pixabay.com/photo/2022/11/23/10/26/mountains-7611804_1280.jpg)] bg-cover">
+      <div className='h-dvh flex flex-row flex-1 items-center justify-center backdrop-blur-sm'>
+      <Card className="h-105 w-1/2 max-w-md mx-2 hover:shadow-xl hover:scale-105">
+        <img
+          src="https://cdn.pixabay.com/photo/2019/08/17/21/54/dungeons-and-dragons-4413056_1280.jpg"
+          alt="dice with notebook"
+          className="relative z-20 aspect-video w-full object-cover"
+        />
         <CardHeader>
-          <CardTitle>Test card</CardTitle>
-          <CardDescription>DM view card description</CardDescription>
+          <CardTitle>Dungeon Master</CardTitle>
+          <CardDescription>
+            WIP system to easily manage and view turn amount, player conditions
+            and condition length, initiative, player stats and monsters
+          </CardDescription>
         </CardHeader>
-        <CardContent>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
-          quasi quas officia fugit fuga fugiat cupiditate quis. Voluptatibus
-          quas nulla qui rerum quia rem, eligendi nihil nam, inventore culpa
-          eveniet?
-        </CardContent>
       </Card>
-      <Card className="w-1/2 max-w-sm mx-2 p-2">
+      <Card className="h-105 w-1/2 max-w-md mx-2 hover:shadow-xl hover:scale-105">
+        <img
+          src="https://cdn.pixabay.com/photo/2023/03/15/12/56/dice-7854512_1280.jpg"
+          alt="pile of d20 dice"
+          className="relative z-20 aspect-video w-full object-cover"
+        />
         <CardHeader>
-          <CardTitle>Test card 2</CardTitle>
-          <CardDescription>Player view card description</CardDescription>
+          <CardTitle>Player</CardTitle>
+          <CardDescription>
+            WIP system to manage player character actions and conditions
+          </CardDescription>
         </CardHeader>
-        <CardContent>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
-          quasi quas officia fugit fuga fugiat cupiditate quis. Voluptatibus
-          quas nulla qui rerum quia rem, eligendi nihil nam, inventore culpa
-          eveniet?
-        </CardContent>
       </Card>
+
+      </div>
     </div>
   );
 }
